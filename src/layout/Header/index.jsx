6c3dbb38a53ad55675b1b720d98c4f17b4b66da1
Rach <90192629+Rachiey@users.noreply.github.com>
2022-04-04 14:44:52 +0100

    add logo to header

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { BackButton } from '../../components';
 import './style.css'
+import logo from '../../../src/kumaflix.png';
+
 
 export const Header = () => {
     return(
         
         <nav>
-            <NavLink className="kumaFlix" exact to='/'>Kumaflix</NavLink>
-            <NavLink exact to='/'>Home</NavLink>
+            <div className='border'>
+                <img className='logo'src={logo} />
+            </div>
+
+            {/* <NavLink className="kumaFlix" exact to='/'>Kumaflix</NavLink>
+            <NavLink exact to='/'>Home</NavLink> */}
             <BackButton className="backButton" />
             <div className="dropdown">
                 <button className="dropbtn">Profiles</button>
